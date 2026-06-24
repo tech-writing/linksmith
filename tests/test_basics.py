3696b712d83dec98ad1b84b5045b05425e308566
Andreas Motl <andreas.motl@panodata.org>
2026-06-25 00:41:11 +0200

    Chore: Fix `test_sphobjinv`, bump version to 2.4

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -11,5 +11,5 @@ def test_sphobjinv():
     objects_inv = "https://sphobjinv.readthedocs.io/en/stable/objects.inv"
     inv = soi.Inventory(url=objects_inv)
     assert inv.project == "sphobjinv"
-    assert inv.version == "2.3"
+    assert inv.version == "2.4"
     assert inv.source_type is soi.SourceTypes.URL
